6f54f0c5da11e1b5950ee89e5412ab1440291739
JoyCuratoR <106856455+JoyCuratoR@users.noreply.github.com>
2022-06-12 12:30:42 -0700

    Update londonmapbot-tweet.R

--- londonmapbot-tweet.R
+++ londonmapbot-tweet.R
@@ -1,38 +1,47 @@
-# Create Twitter token
-londonmapbot_token <- rtweet::create_token(
-  app = "londonmapbot",
-  consumer_key =    Sys.getenv("TWITTER_CONSUMER_API_KEY"),
+library(rvest)
+library(dplyr)
+library(rtweet)
+
+link <- "https://www.brainyquote.com/quote_of_the_day"
+page <- read_html(link)
+
+quote_title <- page %>%
+  html_element(xpath = '//*[contains(concat( " ", @class, " " ),
+               concat( " ", "qotd-h2", " " ))]') %>%
+  html_text()
+
+quote_content <- page %>%
+  html_element(xpath = '//*[contains(concat( " ", @class, " " ),
+               concat( " ", "oncl_q", " " ))]//div') %>%
+  html_text()
+
+quote_author <- page %>%
+  html_element(xpath = '//*[contains(concat( " ", @class, " " ),
+               concat( " ", "oncl_a", " " ))]') %>%
+  html_text()
+
+
+full <- paste0(quote_title, quote_content, quote_author)
+
+cleaned <- gsub('\n', " ", 
+           gsub('Quote of the Day', "Quote of the Day |", full))
+
+
+
+# create the tweet's text
+tweet_text <- paste0(cleaned)
+
+# Create a token containing your Twitter keys
+bot_token <- rtweet::create_token(
+  app = "brainyquotebot",
+  # the name of the Twitter app
+  consumer_key = Sys.getenv("TWITTER_CONSUMER_API_KEY"),
   consumer_secret = Sys.getenv("TWITTER_CONSUMER_API_SECRET"),
-  access_token =    Sys.getenv("TWITTER_ACCESS_TOKEN"),
-  access_secret =   Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+  access_token = Sys.getenv("TWITTER_ACCESS_TOKEN"),
+  access_secret = Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET"),
+  set_renv = FALSE
 )
 
-# Generate random coordinates within specific limits
-lon <- round(runif(1, -0.489, 0.236), 4)
-lon <- format(lon, scientific = FALSE)
-lat <- round(runif(1, 51.28, 51.686), 4)
-
-# Build URL and fetch image from Mapbox API
-img_url <- paste0(
-  "https://api.mapbox.com/styles/v1/mapbox/satellite-v9/static/",
-  paste0(lon, ",", lat),
-  ",15,0/600x400@2x?access_token=",
-  Sys.getenv("MAPBOX_PUBLIC_ACCESS_TOKEN")
-)
-
-# Download the image to a temporary location
-temp_file <- tempfile()
-download.file(img_url, temp_file)
-
-# Build the status message (text and URL)
-latlon_details <- paste0(
-  lat, ", ", lon, "\n",
-  "https://www.openstreetmap.org/#map=17/", lat, "/", lon, "/"
-)
-
-# Post the image to Twitter
-rtweet::post_tweet(
-  status = latlon_details,
-  media = temp_file,
-  token = londonmapbot_token
-)
+# post tweet
+post_tweet(status = tweet_text,
+           token = bot_token)
